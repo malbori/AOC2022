f99a5a600e9c5423b6c26cdda8f559bbe971748c
malbori <mahdialbori@gmail.com>
2022-12-01 00:28:07 -0500

    day 1 done

--- solutions/day_one.py
+++ solutions/day_one.py
@@ -6,20 +6,28 @@ from operator import itemgetter as ig
 import pprint as pp
 import re
 
-from utility.helper import ints
 
 def solve():
 
-    data = open('data_inputs\day01_input.txt', 'r', encoding='utf-8').read()
-    print(data)
+    data = open('data_inputs\day01_input.txt', 'r', encoding='utf-8').read().splitlines()
+    # print(data)
 
-    # convert string to array of nums with helper function
-    nums = ints(data)
-    print(nums)
+    calorie_totals = [0]
+    curr_elf = 0
 
-    # multiple lines of nums
-    # nums = [[ints(l)] for l in d.split("\n")]
+    for i, v in enumerate(data):
+        if v == "":
+            curr_elf +=1
+            calorie_totals.append(0)
+        else:
+            calorie_totals[curr_elf] += int(v)
+    
+    print(max(calorie_totals))
+    
+    #part two answer
+    calorie_totals.sort()
+    print(calorie_totals[-1]+ calorie_totals[-2] + calorie_totals[-3])
 
     return
 
-print(solve())
+solve()
