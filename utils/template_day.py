7e15484607846d1ea6aa71616190f1bbeba40bb5
malbori <mahdialbori@gmail.com>
2022-11-30 20:53:29 -0500

    updated template

--- utils/template_day.py
+++ utils/template_day.py
@@ -6,9 +6,19 @@ from operator import itemgetter as ig
 import pprint as pp
 import re
 
-from utils import *
+from utils.util import *
 
 def solve(d):
     
 
+    # convert string to array of nums with helper function
+    #nums = ints(d)
+
+    #multiple lines of nums
+    # nums = [[ints(l)] for l in d.split("\n")]
+
+
     return
+
+s = ""
+print(solve(s))
