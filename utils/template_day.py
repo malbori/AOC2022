66cd06e644344d290e9ea6ac492e260b14bcbade
malbori <mahdialbori@gmail.com>
2022-11-30 20:46:42 -0500

    added template day as well as util file with helper functions

---
+++ utils/template_day.py
@@ -0,0 +1,14 @@
+import collections as coll
+import datetime as dt
+import itertools as it
+import math
+from operator import itemgetter as ig
+import pprint as pp
+import re
+
+from utils import *
+
+def solve(d):
+    
+
+    return
---
+++ utils/util.py
@@ -0,0 +1,6 @@
+# convert string to int
+def ints(s, sep=None):
+    if isinstance(s, str):
+        return [int(x) for x in s.split(sep)]
+    else:
+        return [int(x) for x in s]
