4a67ae6062b93a11042be2456bbe773b3beb45db
malbori <mahdialbori@gmail.com>
2022-12-01 22:31:44 -0500

    day 2 inital setup commit

---
+++ solutions/day_two.py
@@ -0,0 +1,23 @@
+import collections as coll
+import datetime as dt
+import itertools as it
+import math
+from operator import itemgetter as ig
+import pprint as pp
+import re
+
+from utility import *
+
+def solve():
+
+    data = open('data_inputs\day01_input.txt', 'r', encoding='utf-8').read().splitlines()
+    # convert string to array of nums with helper function
+    # nums = ints(d)
+
+    # multiple lines of nums
+    # nums = [[ints(l)] for l in d.split("\n")]
+
+    return
+
+
+solve()
